007a89d9aae29888325b825d082aa0e4f3a851f7
TheChouzanOne <chouza2598@hotmail.com>
2016-06-13 19:09:45 -0700

    Mostly corrected
    Hace falta corregir cuando se inputea el string vacío

--- project.py
+++ project.py
@@ -2,14 +2,31 @@ import random
 
 number = random.randint(1,5000000)
 
-guess = int(input('You have 20 tries to guess a number between 1 and 5000000: '))
-
 guessesLeft = 20
 
 lb = 1 #lowerbound
 ub = 5000000 #upperbound
 
+
+def check_int(n):
+    for x in n:
+        if x < '0' or x > '9':
+            return False
+    return True
+
+
 while guessesLeft > 0:
+    
+    guess = raw_input('Guess a number between ' + str(lb) + ' ' + str(ub) + ':')
+    
+    while check_int(guess) == False:
+        guess = raw_input('That is not a number, please write it again: ')
+    else:
+        guess = int(guess)
+        
+    
+    print 'Guesses left: ' + str(guessesLeft)
+    
     if number == guess:
         print 'Congratulations! You have won.'
         break
@@ -21,9 +38,11 @@ while guessesLeft > 0:
         if guess > lb:
             lb = guess
         print 'The number you are looking for is bigger than that!'
+    
     guessesLeft -= 1
-    print 'Guesses left: ' + str(guessesLeft)
+
+    
     print number      
-    guess = int(input('Guess a number between ' + str(lb) + ' ' + str(ub) + ':'))
 else:
     print 'Game Over!'
+   
